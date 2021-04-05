3cb29af6035e257075b14f108054d45b1b30240f
chewyang <owenzhouyang98@gmail.com>
2021-04-05 21:03:37 +0800

    direct conversion

---
+++ CG2028AssignS2AY2021/Debug/src/cr_startup_lpc17.d
@@ -0,0 +1,4 @@
+src/cr_startup_lpc17.d: ../src/cr_startup_lpc17.c \
+ C:\Users\Razer\Documents\LPCXpresso_6.1.4_194\workspace1\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h
+
+C:\Users\Razer\Documents\LPCXpresso_6.1.4_194\workspace1\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
